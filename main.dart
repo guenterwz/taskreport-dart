f9fd95b420510d332d5a14ddae58d082dab5dd80
Guenter <guenterwz@gmail.com>
2026-05-30 21:30:43 -0300

    feat: gera relatorio final de tarefas

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+// Base de dados simulada
 final List<Map<String, dynamic>> dadosTarefas = [
   {
     'id': 1,
@@ -64,40 +65,41 @@ final List<Map<String, dynamic>> dadosTarefas = [
   },
 ];
 
-class ItemTrabalho {  //  RF13 – Criar classe base e classe filha
+//  RF13 – Criar classe base e classe filha
+class ItemTrabalho {  
   int id;
-  String titulo;
+  String? titulo;
   ItemTrabalho({required this.id, required this.titulo});
   void exibirResumo() {
     print('Item $id - $titulo');
   }
 }
 
-class Tarefa extends ItemTrabalho {
-  //  RF01 – Transformar mapas em objetos
+//  RF01 – Transformar mapas em objetos
+class Tarefa extends ItemTrabalho {  
   int id;
-  String titulo;
-  String responsavel;
-  String status;
-  String prioridade;
+  String? titulo;
+  String? responsavel;
+  String? status;
+  String? prioridade;
   double valor;
-  int horas;
+  int? horas;
 
-  Tarefa({
+    Tarefa({
     required this.id,
-    required this.titulo,
-    required this.responsavel,
-    required this.status,
-    required this.prioridade,
+    this.titulo,
+    this.responsavel,
+    this.status,
+    this.prioridade,
     required this.valor,
-    required this.horas,
+    this.horas,
   }) : super(id: id, titulo: titulo);
 
   factory Tarefa.doMapa(Map<String, dynamic> m) {
     return Tarefa(
       id: m['id'],
       titulo: m['titulo'] != null
-          ? m['titulo'].toString().trim()
+          ? m['titulo'].toString().trim()  //  RF03 – Remover espaços desnecessários
           : 'Sem título',
       responsavel:
           m['responsavel'] != null //  RF02 – Tratar campos nulos
@@ -120,21 +122,8 @@ class Tarefa extends ItemTrabalho {
   }
 }
 
-/*
-Tarefa converterMapParaTarefa(Map<String, dynamic> m) {
-  return Tarefa(
-    id: m['id'],
-    titulo: m['titulo'] ?? 'Sem título',
-    responsavel: m['responsavel'] ?? 'Não informado',
-    status: m['status'] ?? 'sem status',
-    prioridade: m['prioridade'] ?? 'sem prioridade',
-    valor: converterValor(m['valor']),
-    horas: m['horas'] != null ? int.tryParse(m['horas'].toString()) ?? 0 : 0,
-  );
-}
-*/
-
-double converterValor(dynamic valor) {  //  RF04 – Converter valor monetário para número
+//  RF04 – Converter valor monetário para número
+double converterValor(dynamic valor) {  
   if (valor == null) {
     return 0.0;
   }
@@ -146,14 +135,23 @@ double converterValor(dynamic valor) {  //  RF04 – Converter valor monetário 
   return double.tryParse(valorTexto) ?? 0;
 }
 
+//  RF14 – Aplicar encapsulamento
+class RelatorioTarefas {
+  final List<Tarefa> _tarefas;
+  RelatorioTarefas(List<Tarefa> tarefas): _tarefas = tarefas;
+  int get quantidadeTotal => _tarefas.length;
+}
+
 void main() {
   List<Tarefa> lista = dadosTarefas.map((map) => Tarefa.doMapa(map)).toList();
 
-  for(var item in lista){  //  RF06 – Exibir todas as tarefas convertidas
+  //  RF06 – Exibir todas as tarefas convertidas
+  for(var item in lista){  
       item.exibirResumo();
   }
 
-  print('\nTarefas filtradas por status');//  RF07 – Filtrar tarefas por status
+  //  RF07 – Filtrar tarefas por status
+  print('\nTarefas filtradas por status');
 
   List<Tarefa> concluidas = lista.where((lista) => lista.status == 'concluida').toList();
   print('\nConcluidas:');
@@ -171,4 +169,62 @@ void main() {
   print('\nCancelada:');
   cancelada.forEach((cancelada) => print('- ${cancelada.titulo}'));
 
+  //  RF08 – Somar valores das tarefas concluídas
+  double somaConcluidas = concluidas.map((m) => m.valor).reduce((a, b) => a + b);  
+  print('\nTotal de tarefas concluidas: R\$ $somaConcluidas');
+
+  //  RF09 – Calcular média de valor das tarefas pendentes
+  double somaPendentes = pendente.map((m) => m.valor).reduce((a, b) => a + b);
+  double mediaPendentes = somaPendentes / pendente.length;
+  print(mediaPendentes > 0.0 ? '\nMedia de valor das tarefas pendentes: $mediaPendentes' : '\nNão existem tarefas pendentes para calcular média.');
+
+  //  RF10 – Calcular total de horas por status
+  Map<String, int> tempoPorStatus = {};
+  for (Tarefa t in lista) {
+    String status = t.status ?? 'Sem status';
+    tempoPorStatus[status] = (tempoPorStatus[status] ?? 0) + (t.horas ?? 0);
+  }
+
+  print('\nHoras por status:');
+  tempoPorStatus.forEach((status, horas) => print('- $status: $horas horas'));
+
+  //  RF11 – Identificar tarefas com dados incompletos
+  print('\nTarefas com dados incompletos:');
+  List<String> incompletas = [];
+  for(Map<String, dynamic> t in dadosTarefas){
+    if(t['titulo'] == null) incompletas.add('- ID: ${t['id']} : Titulo ausente.');
+    if(t['responsavel'] == null) incompletas.add('- ID: ${t['id']} : Responsavel ausente.');
+    if(t['status'] == null) incompletas.add('- ID: ${t['id']} : Status ausente.');
+    if(t['valor'] == null) incompletas.add('- ID: ${t['id']} : Valor ausente.');
+    if(t['horas'] == null) incompletas.add('- ID: ${t['id']} : Tempo em horas ausente.');
+    if(t['prioridade'] == null) incompletas.add('- ID: ${t['id']} : Prioridade ausente');
+  }
+  incompletas.forEach((i) => print(i));
+
+  //  RF12 – Exibir status únicos usando Set
+  print('\nLista de status únicos:');
+  Set<String> statusUnicos = {};
+  for (Tarefa t in lista) {
+    statusUnicos.add(t.status ?? 'Sem status');
+  }
+  statusUnicos.forEach((s) => print('- $s'));
+
+  //  RF15 – Gerar relatório final
+  print('=========================================================');
+  RelatorioTarefas r = RelatorioTarefas(lista);
+  
+  print('\nRELATÓRIO FINAL DE TAREFAS\n');
+  print('\nTotal de tarefas analisadas: ${r.quantidadeTotal}');
+  print('Tarefas concluidas: ${concluidas.length}');
+  print('Tarefas pendentes: ${pendente.length}');
+  print('Tarefas em andamento: ${emAndamento.length}');
+  print('Tarefas canceladas: ${cancelada.length}');
+  print('\nValor total das concluidas: R\$ $somaConcluidas');
+  print('Média de valor das pendentes: R\$ $mediaPendentes');
+  int? totalHorasConcluidas = concluidas.map((m) => m.horas).reduce((a, b) => a! + b!);
+  print('Total de horas concluidas: $totalHorasConcluidas');
+  print('\nStatus encontrados:');
+  statusUnicos.forEach((s) => print('- $s'));
+  print('\nTarefas com dados incompletos:');
+  incompletas.forEach((i) => print(i));
 }
